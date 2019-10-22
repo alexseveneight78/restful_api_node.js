f7406a9a56ff6633f3f0d8a16098741d74f28417
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-10-22 22:45:37 +0300

    'parsing_request_paths'

--- index.js
+++ index.js
@@ -6,10 +6,25 @@
 
 // Dependencies 
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string 
 const server = http.createServer(function(req,res){
-    res.end('Hello world\n');
+
+    // Get the URL and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path 
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Send the response 
+    res.end('Hello world\n');    
+    
+    // Log the request path
+    console.log('Request is received on path: ' + trimmedPath);
+    // command 'node index.js' in node shell and 'curl localhost:3000/foo' in cmd shell 
+    // command 'curl localhost:3000/foo/bar/'
 });
 
 // Start the server and have it losten on port 3000 
