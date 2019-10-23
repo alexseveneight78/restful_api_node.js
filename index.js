00dc4c689301003413f538efd2992b46801fce6f
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-10-23 21:35:36 +0300

    'parsing_payloads'

--- index.js
+++ index.js
@@ -7,33 +7,44 @@
 // Dependencies 
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string 
 const server = http.createServer(function(req,res){
 
     // Get the URL and parse it
-    const parsedUrl = url.parse(req.url, true);
+    let parsedUrl = url.parse(req.url, true);
 
     // Get the path 
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+    let path = parsedUrl.pathname;
+    let trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
     // Get the query string as an object --- command 'curl localhost:3000/foo?fizz=buzz
-    const queryStringObject = parsedUrl.query;
+    let queryStringObject = parsedUrl.query;
 
     // Get the HTTP method
-    const method = req.method.toLowerCase();
+    let method = req.method.toLowerCase();
 
     // Get the headers as an object - work with postman
-    const headers = req.headers;
+     headers = req.headers;
 
-    // Send the response 
-    res.end('Hello world\n');    
-    
-    // Log the request path
-    console.log('Request received with these headers',headers);
-    // command 'node index.js' in node shell and 'curl localhost:3000/foo' in cmd shell 
-    // command 'curl localhost:3000/foo/bar/'
+    // Get the payload, if any
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function(){
+        buffer += decoder.end();
+
+        // Send the response 
+        res.end('Hello world\n');    
+        
+        // Log the request path
+        console.log('Request received with this payload',buffer);
+        // command 'node index.js' in node shell and 'curl localhost:3000/foo' in cmd shell 
+        // command 'curl localhost:3000/foo/bar/'
+    });
 });
 
 // Start the server and have it losten on port 3000 
