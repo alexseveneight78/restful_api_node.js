30e8f56afaab9a568859b01767949e4a9d9902de
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-10-25 23:44:55 +0300

    'adding_configuration'

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 // The server should respond to all requests with a string 
 const server = http.createServer(function(req,res){
@@ -76,9 +77,9 @@ const server = http.createServer(function(req,res){
     });
 });
 
-// Start the server and have it losten on port 3000 
-server.listen(3000, function(){
-    console.log('The servers listening on port 3000 now');
+// Start the server
+server.listen(config.port, function(){
+    console.log('The servers listening on port '+config.port+' in '+config.envName+' mode');
 });
 
 // Define the handlers 
